80105bce707770f2ed4042d154426d71c113ad9d
Michael Franzen <franzenmic@gmail.com>
2017-05-15 11:07:50 +0200

    Added widths, workgroups etc. to benchmark.

--- scripts/run-quavis.sh
+++ scripts/run-quavis.sh
@@ -13,7 +13,6 @@ OUTPUT_FILE=output/output.csv
 FUNCTIONS="spherical_area volume"
 
 # arguments
-#TODO: WIDTHS
 RMAX="10000" # DO NOT CHANGE THIS!
 ALPHAS="1 0.1 0.01"
 GEOM_OFF="1 0"
@@ -22,19 +21,20 @@ REPEATS="1000"
 WIDTHS_SPHERICAL="2048 1024 512"
 WIDTHS_CUBE="512 256 128"
 
-TOTAL_COMB=$(echo 1+$(echo $GEOM_OFF | grep -o ' ' | wc -l) | bc)
-TOTAL_COMB=$(echo "$TOTAL_COMB*(1+$(echo $TESS_OFF | grep -o ' ' | wc -l))" | bc)
+# compute total number of combinations for display
+TOTAL_COMB=$(echo 1+$(echo $FUNCTIONS | grep -o ' ' | wc -l) | bc)
 TOTAL_COMB=$(echo "$TOTAL_COMB*(1+$(echo $REPEATS | grep -o ' ' | wc -l))" | bc)
 TOTAL_COMB=$(echo "$TOTAL_COMB*$(ls $TEST_CASES_PATH/*.obj | wc -l)" | bc)
 TOTAL_COMB_SPHERICAL=$(echo "$TOTAL_COMB*(1+$(echo $WIDTHS_SPHERICAL | grep -o ' ' | wc -l))" | bc)
 TOTAL_COMB_SPHERICAL=$(echo "$TOTAL_COMB_SPHERICAL*(1+$(echo $ALPHAS | grep -o ' ' | wc -l))" | bc)
+TOTAL_COMB_SPHERICAL=$(echo "$TOTAL_COMB_SPHERICAL*(1+$(echo $GEOM_OFF | grep -o ' ' | wc -l))" | bc)
+TOTAL_COMB_SPHERICAL=$(echo "$TOTAL_COMB_SPHERICAL*(1+$(echo $TESS_OFF | grep -o ' ' | wc -l))" | bc)
 TOTAL_COMB_CUBE=$(echo "$TOTAL_COMB*(1+$(echo $WIDTHS_CUBE | grep -o ' ' | wc -l))" | bc)
 
-echo $TOTAL_COMB_SPHERICAL
-echo $TOTAL_COMB_CUBE
-
+# Removing old output file
 rm -f $OUTPUT_FILE >> /dev/null
 
+# Running spherical benchmark
 i=1
 echo "MAPPING;FUNCTION;OBJ;REPEATS;RMAX;ALPHA;GEOM_OFF?;TESS_OFF?;WIDTH;X;Y;Z;EXPECTED_RESULT;ACTUAL_RESULT;GRAPHICS_FPS;COMPUTE_FPS;" >> $OUTPUT_FILE
 for function in $FUNCTIONS
@@ -71,6 +71,8 @@ do
   done
 done
 
+# Running cubemap benchmark
+i=1
 for function in $FUNCTIONS
 do
   for repeat in $REPEATS
@@ -85,6 +87,7 @@ do
             do
               for testcase in $TEST_CASES_PATH/*.obj
               do
+                echo "Running Cubemap..." $i/$TOTAL_COMB_CUBE
                 workgroups=$width
                 EXPECTED=$(cat $testcase.$function.out | sed "s/ /;/g")
                 RESULT=$($CUBE_QUAVIS_PATH -s $CUBE_SHADERS_PATH/$function.1.comp.spv -t $CUBE_SHADERS_PATH/$function.2.comp.spv -G $geom -T $tess -x $width -w $workgroups -a $alpha -u $repeat -r $rmax -f $testcase < $testcase.in | tail -n +2 | cut -d " " -f4-7 | sed "s/ /;/g")
@@ -92,6 +95,7 @@ do
                 do
                   echo "CUBE;$function;${testcase##*/};$repeat;$rmax;$geom;$tess;$width;;$line" >> $OUTPUT_FILE
                 done
+                i=$(echo $i+1 | bc)
               done
             done
           done
