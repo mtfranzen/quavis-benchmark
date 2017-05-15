f5c7ec41ac49989546f66f3b7744d6dbe5023ca8
Michael Franzen <franzenmic@gmail.com>
2017-05-15 21:48:11 +0200

    Added automatic shader creation for different r-max, resolutions

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 bash scripts/build-quavis.sh
-bash scripts/build-shaders.sh
 bash scripts/build-testcases.sh
 bash scripts/run-quavis.sh
 python scripts/plot.py
--- scripts/run-quavis.sh
+++ scripts/run-quavis.sh
@@ -13,7 +13,7 @@ OUTPUT_FILE=output/output.csv
 FUNCTIONS="spherical_area volume"
 
 # arguments
-RMAX="10000" # DO NOT CHANGE THIS!
+RMAX="10000.0"
 ALPHAS="0.01"
 GEOM_OFF="0"
 TESS_OFF="0"
@@ -31,8 +31,62 @@ TOTAL_COMB_SPHERICAL=$(echo "$TOTAL_COMB_SPHERICAL*(1+$(echo $GEOM_OFF | grep -o
 TOTAL_COMB_SPHERICAL=$(echo "$TOTAL_COMB_SPHERICAL*(1+$(echo $TESS_OFF | grep -o ' ' | wc -l))" | bc)
 TOTAL_COMB_CUBE=$(echo "$TOTAL_COMB*(1+$(echo $WIDTHS_CUBE | grep -o ' ' | wc -l))" | bc)
 
-# Removing old output file
+# Removing old output file and shaders
 rm -f $OUTPUT_FILE >> /dev/null
+rm -f $SPHERICAL_SHADERS_PATH/auto* >> /dev/null
+
+# Create and compile spherical shaders
+for rmax in $RMAX
+do
+  for function in $FUNCTIONS
+  do
+    for width in $WIDTHS_SPHERICAL
+    do
+      height=$(echo $width/2 | bc)
+      f=$SPHERICAL_SHADERS_PATH/auto_$function.$width.$rmax.1.comp
+      echo "#version 450
+#define WIDTH $width
+#define HEIGHT $height
+#define R_MAX $rmax" > $f
+      cat $SPHERICAL_SHADERS_PATH/$function.1.comp >> $f
+      glslangValidator -V $f -o $f.spv
+
+      f=$SPHERICAL_SHADERS_PATH/auto_$function.$width.$rmax.2.comp
+      echo "#version 450
+#define WIDTH $width
+#define HEIGHT $height
+#define R_MAX $rmax" > $f
+      cat $SPHERICAL_SHADERS_PATH/$function.2.comp >> $f
+      glslangValidator -V $f -o $f.spv
+    done
+  done
+done
+
+# Create and compile cubemap shaders
+for rmax in $RMAX
+do
+  for function in $FUNCTIONS
+  do
+    for width in $WIDTHS_CUBE
+    do
+      f=$CUBE_SHADERS_PATH/auto_$function.$width.$rmax.1.comp
+      echo "#version 450
+#define WIDTH $width
+#define HEIGHT $width
+#define R_MAX $rmax" > $f
+      cat $CUBE_SHADERS_PATH/$function.1.comp >> $f
+      glslangValidator -V $f -o $f.spv
+
+      f=$CUBE_SHADERS_PATH/auto_$function.$width.$rmax.2.comp
+      echo "#version 450
+#define WIDTH $width
+#define HEIGHT $width
+#define R_MAX $rmax" > $f
+      cat $CUBE_SHADERS_PATH/$function.2.comp >> $f
+      glslangValidator -V $f -o $f.spv
+    done
+  done
+done
 
 # Running spherical benchmark
 i=1
@@ -56,7 +110,7 @@ do
                 echo "Running Spherical..." $i/$TOTAL_COMB_SPHERICAL
                 workgroups=$(echo $width/2 | bc)
                 EXPECTED=$(cat $testcase.$function.out | sed "s/ /;/g")
-                RESULT=$($SPHERICAL_QUAVIS_PATH -s $SPHERICAL_SHADERS_PATH/$function.1.comp.spv -t $SPHERICAL_SHADERS_PATH/$function.2.comp.spv -G $geom -T $tess -x $width -w $workgroups -a $alpha -u $repeat -r $rmax -f $testcase < $testcase.in | tail -n +2 | cut -d " " -f4-7 | sed "s/ /;/g")
+                RESULT=$($SPHERICAL_QUAVIS_PATH -s $SPHERICAL_SHADERS_PATH/auto_$function.$width.$rmax.1.comp.spv -t $SPHERICAL_SHADERS_PATH/auto_$function.$width.$rmax.2.comp.spv -G $geom -T $tess -x $width -w $workgroups -a $alpha -u $repeat -r $rmax -f $testcase < $testcase.in | tail -n +2 | cut -d " " -f4-7 | sed "s/ /;/g")
                 paste -d';' <(echo "$EXPECTED") <(echo "$RESULT") | while read line;
                 do
                   echo "SPHERICAL;$function;${testcase##*/};$width;$repeat;$rmax;$geom;$tess;$alpha;$line" >> $OUTPUT_FILE
@@ -86,7 +140,7 @@ do
           echo "Running Cubemap..." $i/$TOTAL_COMB_CUBE
           workgroups=$width
           EXPECTED=$(cat $testcase.$function.out | sed "s/ /;/g")
-          RESULT=$($CUBE_QUAVIS_PATH -s $CUBE_SHADERS_PATH/$function.1.comp.spv -t $CUBE_SHADERS_PATH/$function.2.comp.spv -G $geom -T $tess -x $width -w $workgroups -a $alpha -u $repeat -r $rmax -f $testcase < $testcase.in | tail -n +2 | cut -d " " -f4-7 | sed "s/ /;/g")
+          RESULT=$($CUBE_QUAVIS_PATH -s $CUBE_SHADERS_PATH/auto_$function.$width.$rmax.1.comp.spv -t $CUBE_SHADERS_PATH/auto_$function.$width.$rmax.2.comp.spv -G $geom -T $tess -x $width -w $workgroups -a $alpha -u $repeat -r $rmax -f $testcase < $testcase.in | tail -n +2 | cut -d " " -f4-7 | sed "s/ /;/g")
           paste -d';' <(echo "$EXPECTED") <(echo "$RESULT") | while read line;
           do
             echo "CUBE;$function;${testcase##*/};$width;$repeat;$rmax;;;;;$line" >> $OUTPUT_FILE
